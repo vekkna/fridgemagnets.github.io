d115c89f70f598178941db75ef0986379b8c3467
Neil Crowley <neilccrowley@gmail.com>
2020-03-26 15:31:41 +0000

    Added description of shuffled code

--- public/code/sendToDatabase.js
+++ public/code/sendToDatabase.js
@@ -9,7 +9,9 @@ function sendToDatabase(){
     if($("#inputText").val().length === 0){
         return;
     }
-    var input = {text: $("#inputText").val()};
+    var input = {description: $("#descriptionText").val(),
+        text: $("#inputText").val()};
+    
     var newKey = firebase.database().ref("magnets").push().key;
     $("#shareCode").val(newKey);
     return firebase.database().ref("magnets" + newKey).update(input);
--- public/code/sorting.js
+++ public/code/sorting.js
@@ -9,19 +9,20 @@ $(document).ready((()=>{
     var key = sessionStorage.getItem("key");
     firebase.database().ref(key).once('value', function(snapshot) {
         if(snapshot.val() === null){
-            console.log("nothing found")
             document.getElementById("report").innerHTML = "Sorry, no puzzle for that key found."
         }
         else{
             document.getElementById("report").innerHTML = "Their order is <span id='feedback'>WRONG</span>"
-            this.sourceCode = snapshot.val().text;
-            makeMagnets(snapshot.val().text);
+            makeMagnets(snapshot.val());
         }
       });
 }));
 
-function makeMagnets(text){
-    let arr = text.split('\n');
+function makeMagnets(input){
+    
+    $("#description").text(input.description);
+
+    let arr = input.text.split('\n');
     let stripped = arr.filter((line) => line.length !== 0);
     let trimmed = stripped.map(function(line){
         return line.trim() + " ";
